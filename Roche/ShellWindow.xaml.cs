1eebb3f8c233bc0316d78071445f81dff4a331b0
Brice Lambson <brice@bricelam.net>
2021-12-15 16:00:22 -0800

    Add an icon

--- Roche/ShellWindow.xaml.cs
+++ Roche/ShellWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Roche.Properties;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
 using WinRT.Interop;
@@ -22,8 +25,19 @@ namespace Roche
             Handle = WindowNative.GetWindowHandle(this);
             Title = "Roche";
 
-            // TODO: Set icon
             var handle = (HWND)Handle;
+
+            // Set icon
+            using (var stream = new MemoryStream(Resources.Cockroach))
+            {
+                var smallIcon = new Icon(stream, 16, 16);
+                SendMessage(handle, WM_SETICON, (nuint)ICON_SMALL, smallIcon.Handle);
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var bigIcon = new Icon(stream, 32, 32);
+                SendMessage(handle, WM_SETICON, (nuint)ICON_BIG, bigIcon.Handle);
+            }
 
             // Set size
             var dpi = GetDpiForWindow(handle);
@@ -36,7 +50,6 @@ namespace Roche
                 (int)(Width * scale),
                 (int)(Height * scale),
                 SET_WINDOW_POS_FLAGS.SWP_NOMOVE);
-
 
             // Remove minimize and maximize buttons
             var style = (WINDOW_STYLE)GetWindowLong(handle, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
